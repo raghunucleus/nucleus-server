9836309b008a054783cf3eebb8ccce15e735aa81
raghunucleus <softwaredevelopment@raghuenggcollege.in>
2026-05-06 18:06:31 +0530

    docker files updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 4000
+  Logger.log(`Server is Running in PORT: ${PORT}`)
+  await app.listen(PORT);
 }
 bootstrap();
